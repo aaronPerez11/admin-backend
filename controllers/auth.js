5320640f3034895e333920ed1ae348ef3eaa9568
aaron.perez <aaron.perez@isita.com.mx>
2022-09-14 18:28:39 -0500

    Fin sección 10

---
+++ controllers/auth.js
@@ -0,0 +1,50 @@
+const { response } = require('express');
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
+
+const login = async( req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        //verificar email
+        const usuarioDB = await Usuario.findOne({ email });
+
+        if(!usuarioDB){
+            return res.status(404).json({
+                ok:false,
+                msg:'Email invalido'
+            });
+        }
+
+        //verificar contraseña
+        const validaPassword = bcrypt.compareSync( password, usuarioDB.password);
+        if(!validaPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Contraseña incorrecta'
+            });
+        }
+
+        //Generar TOKEN
+        const token =  await generarJWT( usuarioDB.id );
+
+        res.json({
+            ok: true,
+            token
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador'
+        })
+    }
+}
+
+module.exports = {
+    login
+}
--- index.js
+++ index.js
@@ -8,8 +8,10 @@ require('dotenv').config();
 const app = express();
 
 //Configurar cors
-
 app.use(cors());
+
+//Lectura y parseo de body
+app.use(express.json());
 
 //DB
 dbConnection();
@@ -17,13 +19,9 @@ dbConnection();
 //mean_user | 4vJ5qZ4SWPwL0HIz
 
 //Rutas
-app.get( '/', (req, res) => {
+app.use( '/api/usuarios', require('./routes/usuarios'));
+app.use( '/api/login', require('./routes/auth'));
 
-    res.json({
-        ok:true,
-        msg: 'Hola mundo'
-    });
-});
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto '+ 3000);
