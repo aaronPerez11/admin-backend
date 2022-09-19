941b239414bdc917135175bbc5c6fd3f0a4a47df
aaron.perez <aaron.perez@isita.com.mx>
2022-09-19 17:46:57 -0500

    Seccion 12- Google

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 //Configurar cors
 app.use(cors());
 
+//Carpeta publica
+app.use( express.static('public'));
+
 //Lectura y parseo de body
 app.use(express.json());
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 Ruta: /api/login
 */
 const { Router } = require('express');
-const { login } = require('../controllers/auth');
+const { login, loginGoogle } = require('../controllers/auth');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -15,6 +15,14 @@ router.post( '/',
         validarCampos
     ],
     login
+)
+
+router.post( '/google', 
+    [
+        check('token','El toke de google es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    loginGoogle
 )
 
 
