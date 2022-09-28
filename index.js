47900b1dcf7a479fbd42ad4ff684446b9cc6b0e9
aaron.perez <aaron.perez@isita.com.mx>
2022-09-28 10:57:10 -0500

    SPA control

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
+const path = require('path');
+
 require('dotenv').config();
 
 
@@ -28,6 +30,11 @@ app.use( '/api/medicos', require('./routes/medicos'));
 app.use( '/api/login', require('./routes/auth'));
 app.use( '/api/todo', require('./routes/busquedas'));
 app.use( '/api/upload', require('./routes/uploads'));
+
+// Rutas de angular
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html'))
+})
 
 
 app.listen(process.env.PORT || 5000, () => {
